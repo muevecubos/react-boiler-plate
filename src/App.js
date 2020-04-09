f8b9c55358456a2cc3843f1761941888a1b783c0
Edgard Massot <emassot@gmail.com>
2020-04-09 18:32:39 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {hot} from 'react-hot-loader';
 
 function App() {
+  const [counter,setCounter] = useState(0);
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit {counter} <code>src/App.js</code> and  to reload.
         </p>
-        <a
+        <button onClick={()=>setCounter(counter+1)}>hola</button>
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Neural network powr
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default hot(module)(App);
